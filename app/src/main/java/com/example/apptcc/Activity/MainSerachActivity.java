63a1f22898b6b2b820879f9c1275b1c222c85e4d
reginaldo-basilio <reginaldobasiliocunha@gmail.com>
2021-06-23 07:37:05 -0300

    Colocando borda no spinners e melhorando a tela de busca

--- app/src/main/java/com/example/apptcc/Activity/MainSerachActivity.java
+++ app/src/main/java/com/example/apptcc/Activity/MainSerachActivity.java
@@ -2,7 +2,9 @@ package com.example.apptcc.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -12,10 +14,16 @@ public class MainSerachActivity extends AppCompatActivity {
 
     private Spinner spState, spCity, spCategory, spSubCategory;
 
-    private String[] stateListSpinner = new String []{"Selecione um estado", "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+    private String[] stateListSpinner = new String []{"Selecione um Estado", "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
             "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará",
             "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul",
             "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"};
+
+    private String[] cityListSpinner = new String []{"Selecione uma Cidade"};
+
+    private String[] categoryListSpinner = new String []{"Selecione uma Categoria"};
+
+    private String[] subCategoryListSpinner = new String []{"Selecione uma Subcategoria"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,7 @@ public class MainSerachActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_serach);
 
         spState = (Spinner) findViewById(R.id.spState);
-        spCity = (Spinner) findViewById(R.id.spState);
+        spCity = (Spinner) findViewById(R.id.spCity);
         spCategory = (Spinner) findViewById(R.id.spCategory);
         spSubCategory = (Spinner) findViewById(R.id.spSubCategory);
 
@@ -31,12 +39,23 @@ public class MainSerachActivity extends AppCompatActivity {
 
     }
 
+    @SuppressLint("WrongConstant")
     private void fillStateSpinner(){
         //spState.setAdapter(new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item, stateListSpinner));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_states, stateListSpinner);
-        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spState.setAdapter(adapter);
+        ArrayAdapter<String> stateAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout_with_border, stateListSpinner);
+        spState.setAdapter(stateAdapter);
+
+        ArrayAdapter<String> cityAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout_with_border, cityListSpinner);
+        spCity.setAdapter(cityAdapter);
+
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout_with_border, categoryListSpinner);
+        spCategory.setAdapter(categoryAdapter);
+
+        ArrayAdapter<String> subCategoryAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout_with_border, subCategoryListSpinner);
+        spSubCategory.setAdapter(subCategoryAdapter);
+
+
     }
 
 }
